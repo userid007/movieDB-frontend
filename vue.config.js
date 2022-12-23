70c2e761fe8791e337871085864cc98577eeaf50
userid007 <shindeakshay7499@gmail.com>
2022-12-23 12:11:23 +0530

    minor fix

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+  proxy: 'http://35.239.249.194',
+  }
+  }
+  
